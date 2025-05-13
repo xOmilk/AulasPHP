9e107e376580a4bc24de5251d0f09699026204ca
xOmilk <leite46052@gmail.com>
2025-05-13 00:23:46 -0300

    Criação do index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela inicial</title>
+</head>
+
+<body>
+    <?php
+
+    $nóme='Antonio gabriel';
+
+    print $nóme;
+
+    echo "<p>Olá mundo no paragrafo echo</p>";
+    print("Hello world utilizando o print");
+    ?>
+</body>
+
+</html>
